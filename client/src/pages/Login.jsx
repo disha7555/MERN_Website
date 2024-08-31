2c60022e5596c424b899d5d5cb17d42b211e63d4
disha7555 <190320107555.ce.disha.shah@gmailcom>
2024-08-31 15:48:04 +0530

    client folder work

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+//import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  //const navigate = useNavigate();
+
+  // let handle the input field value
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+
+  // handle form on submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image reg-img">
+                <img
+                  src="/images/login.jpg"
+                  alt="a nurse with a cute look"
+                  width="400"
+                  height="500"
+                />
+              </div>
+              {/* our main login code  */}
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">Login form</h1>
+                <br />
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="text"
+                      name="email"
+                      value={user.email}
+                      onChange={handleInput}
+                      placeholder="email"
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      value={user.password}
+                      onChange={handleInput}
+                      placeholder="password"
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Login Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
+};
